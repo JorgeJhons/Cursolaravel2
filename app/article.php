d0c2eb1185a6fc355244853d0010c37c1042deea
Jorge Fernandez <jfernandezcolli@gmail.com>
2017-12-12 22:57:30 -0600

    Se agrego la relacion entre usuarios y categorias.

--- app/article.php
+++ app/article.php
@@ -12,4 +12,8 @@ class article extends Model
     public function category(){
     	return $this->belongsTo('App\category');
     }
+
+    public function user(){
+    	return $this->belongsTo('App\User');
+    }
 }
