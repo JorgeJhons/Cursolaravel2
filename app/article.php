79dc1d483b0a41517727dba43b6d05fd940a2da6
Jorge Fernandez <jfernandezcolli@gmail.com>
2017-12-12 22:49:09 -0600

    SE agrega la relacion inversa entre la tabla de articulo y categoria.

--- app/article.php
+++ app/article.php
@@ -8,4 +8,8 @@ class article extends Model
 {
     protected $table = "articles";
     protected $filable=['title','content','category_id','user_id'];
+
+    public function category(){
+    	return $this->belongsTo('App\category');
+    }
 }
