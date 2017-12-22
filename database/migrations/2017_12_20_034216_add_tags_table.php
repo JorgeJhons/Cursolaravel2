e45d3d83e05dc4d1dda31568d3dbf763aee34e6a
Jorge Fernandez <jfernandezcolli@gmail.com>
2017-12-21 20:59:42 -0600

    Se realiza de nuevo la migracion de la tabla de tags para corregir errores al momento de relacionar.

--- database/migrations/2017_12_20_034216_add_tags_table.php
+++ database/migrations/2017_12_20_034216_add_tags_table.php
@@ -15,6 +15,17 @@ class AddTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('article_tag',function(){
+            $table->increments('id');
+            $table->integer('article_id')->unsigned();
+            $table->integer('tag_id')->unsigned();
+
+            $table->foreign('article_id')->references('id')->on('articles');
+            $table->foreign('tag_id')->references('id')->on('tags');
             $table->timestamps();
         });
     }
