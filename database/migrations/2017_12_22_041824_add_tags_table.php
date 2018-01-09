85173f81e6bc0fd93bf93f48680d674faf2aad7c
Jorge Fernandez <jfernandezcolli@gmail.com>
2018-01-08 22:58:46 -0600

    Se ingresan los primeros datos relacionando un tag con los articulos.

--- database/migrations/2017_12_22_041824_add_tags_table.php
+++ database/migrations/2017_12_22_041824_add_tags_table.php
@@ -13,7 +13,6 @@ class AddTagsTable extends Migration
      */
     public function up()
     {
-
         Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
