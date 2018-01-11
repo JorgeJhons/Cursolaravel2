60c3bb5755ed95e8f3a95ca29c359320a34fe52a
Jorge Fernandez <jfernandezcolli@gmail.com>
2018-01-10 23:30:55 -0600

    Se agrega el codigo para visualizar los campos requeridos.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,20 @@
-hola jorge fernandez
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}">
+	<title>{{$reportage->title}}</title>
+</head>
+<body>
+	<h1>PORESTO ONLINE</h1>
+	<h2>{{$reportage->title}}</h2>
+	<hr>
+	{{$reportage->content}}
+	<hr>
+	{{$reportage->user->name}}|{{$reportage->category->name}}|
 
-<?php echo $reportage->title ?>
+	@foreach($reportage->tags as $tag)
+		{{$tag->name}}
+	@endforeach
+</body>
+</html>
