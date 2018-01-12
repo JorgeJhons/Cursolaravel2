acd606fe631753d9c705033b2a1973969107f3f0
Jorge Fernandez <jfernandezcolli@gmail.com>
2018-01-11 20:40:44 -0600

    Se agrega el directorio donde se guardaran las plantillas de la seccion del administrador.

---
+++ resources/views/admin/template/main.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/bootstrap.min.css')}}">
+</head>
+<body>
+
+</body>
+</html>
