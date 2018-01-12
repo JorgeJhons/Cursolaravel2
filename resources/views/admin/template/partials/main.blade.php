c8042bcc888d30b5f53f52d7e717ff5cf2234f76
Jorge Fernandez <jfernandezcolli@gmail.com>
2018-01-11 22:10:47 -0600

    Se crea el templates/partials para dividir las plantillas.

---
+++ resources/views/admin/template/partials/main.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title></title>
+	<link rel="stylesheet" href="{{asset('plugins/bootstrap/css/bootstrap.css')}}">
+</head>
+<body>
+	<section>
+		@yield('content')
+	</section>
+	<script src="{{asset('plugins/bootstrap/js/bootstrap.js')}}"></script>
+	<script src="{{asset('plugins/bootstrap/js/jquery.js')}}"></script>
+
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,10 @@
 @extends('admin.template.main')
 
-<h1>Hola desde bootstrap.</h1>
-<a href="" class="btn btn-success"> Boton redondo verde.</a>
+@section('title')
+ HOME
+@endsection
+
+@section('content')
+    <h1>Hola desde bootstrap.</h1>
+    <a href="" class="btn btn-success"> Boton redondo verde.</a>
+@endsection
