a3515d7ea5f7a8b3834f448f25ed5256bbe4c69a
Jorge Fernandez <jfernandezcolli@gmail.com>
2018-01-13 11:03:11 -0600

    Se agrega parte del codigo para probar el sluggable.

--- app/article.php
+++ app/article.php
@@ -3,9 +3,32 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class article extends Model
 {
+
+    use Sluggable;
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'title-slug' => [
+                'source' => 'title'
+            ],
+            'article-slug'=>[
+                'source'=>['article.content','article.category_id','article.user_id'],
+
+                'separator'=>'_'
+            ],
+        ];
+    }
+
     protected $table = "articles";
     protected $filable=['title','content','category_id','user_id'];
 
