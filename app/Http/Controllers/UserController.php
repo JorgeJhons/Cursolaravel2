1e7012d36228a9c2848988f993dbad159f225666
Jorge Fernandez <jfernandezcolli@gmail.com>
2018-01-25 22:40:52 -0600

    Se prueba el funcionamiento de las rutas.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,4 +7,13 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     //
+    public function index(){
+
+    }
+
+    //
+    public function create(){
+    	dd('Por fin funciono.');
+    }
+
 }
